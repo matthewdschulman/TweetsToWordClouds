4869e5bc04425889b0d8ca780735fdd20d1eaf77
Matthew Schulman <matthewdschulman@gmail.com>
2014-04-28 11:22:08 -0400

    output files working

--- src/TwitterScraper.java
+++ src/TwitterScraper.java
@@ -1,6 +1,16 @@
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,8 +20,9 @@ public class TwitterScraper {
      * Usage: java twitter4j.examples.search.SearchTweets [query]
      *
      * @param args search query
+     * @throws IOException 
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Welcome to the Word Cloud Generator!");
         LinkedList<String> userQueries = AskUser.getQueries();
         ConfigurationBuilder cb = new ConfigurationBuilder();
@@ -24,22 +35,34 @@ public class TwitterScraper {
         Twitter twitter = tf.getInstance();
         //iterates through every one of the user's search terms
         while (userQueries != null) {
+        	PrintWriter out = null;
 	        try {
-	            Query query = new Query(userQueries.getFirst());
-	            QueryResult result;
+	        	String firstTerm = userQueries.getFirst();	                     
+	            try {
+	                //create an output file	         
+	            	System.out.println("Here 1");
+	                File logFile = new File(firstTerm.replace(" ", ""));
+	                out = new PrintWriter(new FileWriter(logFile)); 	                             
+		        } catch (Exception e) {
+		            e.printStackTrace();
+		        }
+	            
+	            Query query = new Query(firstTerm);	            
+	            QueryResult result;	   
 	            do {
 	                result = twitter.search(query);
 	                List<Status> tweets = result.getTweets();
 	                for (Status tweet : tweets) {
-	                    System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
+	                    out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
 	                }
-	            } while ((query = result.nextQuery()) != null);
-	            System.exit(0);
+	            } while ((query = result.nextQuery()) != null);	 
+	            
 	        } catch (TwitterException te) {
 	            te.printStackTrace();
 	            System.out.println("Failed to search tweets: " + te.getMessage());
 	            System.exit(-1);
 	        }
+	        out.close();
 	        userQueries.remove();
         }
     }
