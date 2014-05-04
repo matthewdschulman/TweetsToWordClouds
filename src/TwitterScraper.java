6325729ed7a808ceaccccb60284661a4999da548
Matthew Schulman <matthewdschulman@gmail.com>
2014-05-04 03:04:40 -0400

    hash map output works but not sorted

--- src/TwitterScraper.java
+++ src/TwitterScraper.java
@@ -1,19 +1,16 @@
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.Map;
 
 
 public class TwitterScraper {
@@ -23,8 +20,6 @@ public class TwitterScraper {
      * @throws IOException 
      */
     public static void main(String[] args) throws IOException {
-    	int whileCounter = 0;
-    	int getFirstCounter = 0;
     	int twitterSearchCounter = 0;
         System.out.println("Welcome to the Word Cloud Generator!");
         LinkedList<String> userQueries = AskUser.getQueries();
@@ -41,20 +36,26 @@ public class TwitterScraper {
         String curTerm = "";
         boolean firstTimeThrough = true;
         while (numOfQueries > 0) {
+        	//create hashmap for word frequencies
+        	/*Map<String, Integer> wordFreq = new TreeMap<String, Integer>();
+        	ValueComparator comparator =  new ValueComparator(wordFreq);
+            TreeMap<String, Integer> sorted_map = new TreeMap<String,Integer>(comparator);*/
+        	LinkedHashMap<String, Integer> wordFreq = new LinkedHashMap<String, Integer>();
         	numOfQueries--;
         	if (firstTimeThrough) {
         		System.out.print("Please wait one moment...");
         	}
         	firstTimeThrough = false;
-        	whileCounter++;
-        	PrintWriter out = null;
+        	PrintWriter outText = null;
+        	PrintWriter outHash = null;
 	        try {
 	        		curTerm = userQueries.remove();
-	        		getFirstCounter++;
 	            try {
 	                //create an output file	         
-	                File logFile = new File(curTerm.replace(" ", ""));
-	                out = new PrintWriter(new FileWriter(logFile)); 	
+	                File logFileText = new File(curTerm.replace(" ", ""));
+	                File logFileTextHash = new File(curTerm.replace(" ", "") + "Hash");
+	                outText = new PrintWriter(new FileWriter(logFileText)); 	
+	                outHash = new PrintWriter(new FileWriter(logFileTextHash));
 	                //test that out is working
 		        } catch (Exception e) {
 		            e.printStackTrace();
@@ -68,7 +69,20 @@ public class TwitterScraper {
 	                twitterSearchCounter++;
 	                List<Status> tweets = result.getTweets();
 	                for (Status tweet : tweets) {
-	                    out.print(tweet.getText() + " ");
+	                	String curTweet = tweet.getText().toLowerCase() + " ";
+	                	outText.print(curTweet);
+	                	String[] parts = curTweet.split(" ");
+	                	//add substrings to hashmap
+	                	for (String subWord : parts) {
+	                		if (wordFreq.containsKey(subWord)) {
+	                			//increase value by one
+	                			wordFreq.put(subWord, wordFreq.get(subWord) + 1);
+	                		}
+	                		else {
+	                			//first occurence of the word
+	                			wordFreq.put(subWord, 1);
+	                		}
+	                	}	                    
 	                }
 	            } while ((query = result.nextQuery()) != null && twitterSearchCounter < 10);	 
 	            
@@ -77,8 +91,39 @@ public class TwitterScraper {
 	            System.out.println("Failed to search tweets: " + te.getMessage());
 	            System.exit(-1);
 	        }
-	        out.close();
+	        outText.close();
+	        //sorted_map.putAll(wordFreq);
+	        //print hashmap to outHash file
+	        //Iterator iterator = sorted_map.keySet().iterator();  
+	        
+	        /*while (iterator.hasNext()) {  
+	           String key = iterator.next().toString();  
+	           String value = sorted_map.get(key).toString();  	           
+	           outHash.println(key + ", " + value);  
+	        }  */
+	        Iterator<String> iterator = wordFreq.keySet().iterator();
+	        while (iterator.hasNext()) {
+	        	String key = iterator.next().toString();  
+		        String value = wordFreq.get(key).toString();  	           
+		        outHash.println(key + ", " + value);  
+	        }
+	        outHash.close();
         }
     Reset.reset();    
+    }
+}
+
+class ValueComparator implements Comparator<String> {
+    Map<String, Integer> base;
+    public ValueComparator(Map<String, Integer> base) {
+        this.base = base;
+    }
+    // Note: this comparator imposes orderings that are inconsistent with equals.    
+    public int compare(String a, String b) {
+        if (base.get(a) >= base.get(b)) {
+            return -1;
+        } else {
+            return 1;
+        } // returning 0 would merge keys
     }
 }
